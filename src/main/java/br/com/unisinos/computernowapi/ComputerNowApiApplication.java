12a31d43c3e535af0ea19b262b0e95578014d7a6
Anderson Cossul <anderson_cossul@hotmail.com>
2021-08-23 17:58:48 -0300

    first commit spring initializr

---
+++ src/main/java/br/com/unisinos/computernowapi/ComputerNowApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.unisinos.computernowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerNowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerNowApiApplication.class, args);
+	}
+
+}
